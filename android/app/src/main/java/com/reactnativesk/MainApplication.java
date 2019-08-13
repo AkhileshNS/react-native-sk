d494a0e88b5e715e2db2b5ad16099e95c7da8e5c
AkhileshNS <nsakhilesh02@gmail.com>
2019-08-14 00:56:27 +0530

    Removed react-native-config

--- android/app/src/main/java/com/reactnativesk/MainApplication.java
+++ android/app/src/main/java/com/reactnativesk/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.masteratul.exceptionhandler.ReactNativeExceptionHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
