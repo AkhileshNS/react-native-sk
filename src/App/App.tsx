7e72bf5ff5f46834beceaa73907e8fef396f7ef7
AkhileshNS <nsakhilesh02@gmail.com>
2020-07-16 11:43:34 +0530

    Added Theming

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,21 +3,25 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import screens from 'src/screens';
 import {Store} from 'src/App/App.store';
+import {Provider as PaperProvider} from 'react-native-paper';
+import theme from 'src/global/theme';
 
 const Stack = createStackNavigator();
 
 const App = () => (
-  <Store.Provider>
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Demo"
-        screenOptions={{headerShown: false}}>
-        {screens.map((screen, i) => (
-          <Stack.Screen {...screen} key={'Screen ' + i} />
-        ))}
-      </Stack.Navigator>
-    </NavigationContainer>
-  </Store.Provider>
+  <PaperProvider theme={theme}>
+    <Store.Provider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Demo"
+          screenOptions={{headerShown: false}}>
+          {screens.map((screen, i) => (
+            <Stack.Screen {...screen} key={'Screen ' + i} />
+          ))}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Store.Provider>
+  </PaperProvider>
 );
 
 export default App;
